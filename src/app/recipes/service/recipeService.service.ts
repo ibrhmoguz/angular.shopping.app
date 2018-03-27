1d0ad96fe4fa6927beabbc54bb954b791fba456b
Ibrahim OGUZ <ibrahimoguz.ie@gmail.com>
2018-03-27 12:10:53 +0300

    Ingredients added to shopping list.

--- src/app/recipes/service/recipeService.service.ts
+++ src/app/recipes/service/recipeService.service.ts
@@ -1,16 +1,40 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { Ingredient } from '../../shared/ingredient.model';
+import { ShoppingService } from '../../shopping-list/service/shoppingService.service';
 
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('Baklava', 'This is simply a test1', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlyzIcrYJY8ZyLdsMS1PzOfeNuIhvmZIGXM8LTwozWqpZsYscJuA'),
-        new Recipe('Waffle', 'This is simply a test2', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFXaV5vKd3FLcWPhw0Q-p2q6_8tbxTMR8aB2m5ZekDVD15zxQ7Pw')
+        new Recipe(
+            'Baklava',
+            'This is simply a test1',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlyzIcrYJY8ZyLdsMS1PzOfeNuIhvmZIGXM8LTwozWqpZsYscJuA',
+            [
+                new Ingredient('Şeker', 1),
+                new Ingredient('Yufka', 20),
+                new Ingredient('Antep Fıstık', 10)
+            ]),
+        new Recipe(
+            'Waffle',
+            'This is simply a test2',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFXaV5vKd3FLcWPhw0Q-p2q6_8tbxTMR8aB2m5ZekDVD15zxQ7Pw',
+            [
+                new Ingredient('Şeker', 1),
+                new Ingredient('Çilek', 20),
+                new Ingredient('Muz', 10),
+                new Ingredient('Çikolota Sosu', 10)
+            ])
     ];
-    constructor() { }
+    constructor(private shoppingService: ShoppingService) { }
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.shoppingService.addIngredients(ingredients);
     }
 }
