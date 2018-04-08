c5b28ca8d3ec69969f8c597d85d3a939c6b6dc8e
Ibrahim OGUZ <ibrahimoguz.ie@gmail.com>
2018-04-08 22:18:04 +0300

    RecipeModule added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,40 +5,30 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingService } from './shopping-list/service/shoppingService.service';
 import { AppRouting } from './appRouting.module';
-import { RecipeStartComponent } from './recipes/recipeStart/recipeStart.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeService } from './recipes/service/recipeService.service';
 import { DataStorageService } from './shared/dataStorageService.service';
+import { RecipesModule } from './recipes/recipe.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent
+    DropdownDirective
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     AppRouting,
-    ReactiveFormsModule
+    RecipesModule
   ],
   providers: [ShoppingService, RecipeService, DataStorageService],
   bootstrap: [AppComponent]
